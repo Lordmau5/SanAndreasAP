e69835e9c20af1bce7d73a8d21a8cc4e79a18f4e
Lordmau5 <mail@lordmau5.com>
2026-09-08 19:58:37 +0200

    Add new option to disable horse betting
    They are exploitable with pause buffering, this way we can disable them through code so players aren't able to abuse that. Still keeping it as a config option though

--- source/Mod.cpp
+++ source/Mod.cpp
@@ -48,6 +48,7 @@ void Mod::start()
     StreetRaceUnlock::update(m_streetRacesUnlocked);
     StreetRaceUnlock::updateDrivingSchoolBlip();
     WangCarsUnlock::update(m_wangCarsUnlocked);
+    HorseBettingHandler::install();
 
     m_apSocket.update();
     pollDeathLink();
